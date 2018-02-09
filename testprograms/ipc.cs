2f92cdf2d6c9c7c06c920ce2492f8cbdf36ffbfa
Perry Werneck <perry.werneck@gmail.com>
2018-02-09 17:27:26 -0200

    Identificando problema na quebra de linha ao obter a tela em C#

---
+++ testprograms/ipc.cs
@@ -0,0 +1,62 @@
+/*
+ * "Software pw3270, desenvolvido com base nos códigos fontes do WC3270  e X3270
+ * (Paul Mattes Paul.Mattes@usa.net), de emulação de terminal 3270 para acesso a
+ * aplicativos mainframe. Registro no INPI sob o nome G3270.
+ *
+ * Copyright (C) <2008> <Banco do Brasil S.A.>
+ *
+ * Este programa é software livre. Você pode redistribuí-lo e/ou modificá-lo sob
+ * os termos da GPL v.2 - Licença Pública Geral  GNU,  conforme  publicado  pela
+ * Free Software Foundation.
+ *
+ * Este programa é distribuído na expectativa de  ser  útil,  mas  SEM  QUALQUER
+ * GARANTIA; sem mesmo a garantia implícita de COMERCIALIZAÇÃO ou  de  ADEQUAÇÃO
+ * A QUALQUER PROPÓSITO EM PARTICULAR. Consulte a Licença Pública Geral GNU para
+ * obter mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com este
+ * programa; se não, escreva para a Free Software Foundation, Inc., 51 Franklin
+ * St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Este programa está nomeado como sample.cs e possui - linhas de código.
+ *
+ * Contatos:
+ *
+ * perry.werneck@gmail.com	(Alexandre Perry de Souza Werneck)
+ * erico.mendonca@gmail.com	(Erico Mascarenhas Mendonça)
+ *
+ */
+
+using pw3270;
+
+class ipc {
+
+	static void Main(string[] args) {
+
+		pw3270.Session host = new pw3270.Session("pw3270:A");
+
+		System.Console.WriteLine("Using pw3270 version " + host.Version + " revision " + host.Revision);
+		System.Console.WriteLine("Screen size is " + host.Width + "x" + host.Height + " (" + host.Length + ")");
+
+		// host.CharSet = "ISO-8859-1";
+
+		if(host.Connected) {
+
+			System.Console.WriteLine("Connected to " + host.Url);
+			System.Console.WriteLine("Wait for ready returned " + host.WaitForReady(5));
+
+			System.Console.WriteLine(host.GetStringAt(1,1,1920));
+			// System.Console.WriteLine(host);
+
+		} else {
+
+			System.Console.WriteLine("Host is disconnected");
+
+		}
+
+		System.Console.WriteLine("Error: " + host.Error);
+
+
+	}
+
+}
