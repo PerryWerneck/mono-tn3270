c3c6f306b8437e67e1624dc8d18353c5b27aaaa0
Perry Werneck <perry.werneck@gmail.com>
2019-04-22 10:58:28 -0300

    Updating gitsync.sh

--- gitsync.sh
+++ gitsync.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 #
 # https://help.github.com/articles/syncing-a-fork/
-#
 # https://help.github.com/articles/configuring-a-remote-for-a-fork/
+# https://www.opentechguides.com/how-to/article/git/177/git-sync-repos.html
 #
-# git remote add bitbucket https://bitbucket.org/pw3270/pw3270-sharp.git
+# Setup:
 #
+# git remote add --mirror=fetch secondary https://secondary_repo_url/secondary_repo.git
+# git fetch origin
+# git push secondary --all
+#
+#
+
+git push
+
 git fetch origin
 git checkout master
 git merge origin/master
 
-git push bitbucket
+for repo in $(git remote -v | grep -v origin | grep "(push)" | awk '{print $1}')
+do
+	echo "Updating ${repo} ..."
+	git push ${repo}
+done
+
 
